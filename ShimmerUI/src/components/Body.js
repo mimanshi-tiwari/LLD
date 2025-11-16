5290067e088e2ea400fb5a8c3996482e9c45fbce
Mimanshi Tiwari <tiwari.mimanshi@gmail.com>
2025-11-16 16:29:10 +0530

    infinite scroll

--- ShimmerUI/src/components/Body.js
+++ ShimmerUI/src/components/Body.js
@@ -3,14 +3,15 @@ import MemeCard from './MemeCard'
 import Shimmer from './Shimmer'
 
 const Body = () => {
-    const [memes, setMemes] = useState(null)
+    const [memes, setMemes] = useState([])
     const [loading, setLoading] = useState(true)
 
     const fetchMemes = async () => {
         try {
+            setLoading(true)
             const response = await fetch('https://meme-api.com/gimme/20')
             const memesResponse = await response.json()
-            setMemes(memesResponse.memes)
+            setMemes((memes) => [...memes, ...memesResponse.memes])
             setLoading(false)
         } catch (error) {
             console.error('Error fetching memes:', error)
@@ -18,12 +19,23 @@ const Body = () => {
     }
 
     useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
         fetchMemes()
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
     }, [])
+
+    const handleScroll = () => {
+        if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+            fetchMemes()
+        }
+    } 
     return (
         <div className='flex flex-wrap justify-center'>
-            {loading && Array.from({ length: 20 }).map((_, index) => <Shimmer key={index} />)}
-            {memes && memes.map((meme, index) =>  <MemeCard key={index + meme.ups + meme.title} data={meme} />)}
+            {memes.length > 1 && memes.map((meme, index) =>  <MemeCard key={index + meme.ups + meme.title} data={meme} />)}
+            {loading && Array.from({ length: 5 }).map((_, index) => <Shimmer key={index} />)}
         </div>
     )
 }
--- ShimmerUI/src/components/MemeCard.js
+++ ShimmerUI/src/components/MemeCard.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const MemeCard = (data) => {
-    const {title, url, author, previews} = data.data
-    console.log(data)
+    const {title, url, author} = data.data
   return (
-    <div className='p-4 m-2 border rounded-lg shadow-lg transition-all duration-300 flex gap-4 flex-col' >
-        <h2 className='text-red-600 bold'>{title}</h2>
-        <img className="w-64 h-64" src={url} alt={title} />
-        <p className='bold'>Author: {author}</p>      
+    <div className='p-4 m-2 border rounded-lg shadow-lg transition-all duration-300 flex gap-4 flex-col w-56' >
+        <h2 className='text-red-600 bold text-sm'>{title}</h2>
+        <img className="w-48 h-48" src={url} alt={title} />
+        <p className='bold text-sm'>Author: {author}</p>      
     </div>
   )
 }
